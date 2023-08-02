a263096692386f2b6462191d9cd9b84ee45cc20b
Henry Meliato <hmagalhaesmeliatto@gmail.com>
2023-08-02 11:09:23 -0300

    Criação do repositório

---
+++ netbeansgit/src/main/java/pacote/Netbeansgit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pacote;
+
+/**
+ *
+ * @author hmaga
+ */
+public class Netbeansgit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
